70b5efa069204c2c98513e6ce9a490811f9c04a4
lovemelvm <nastyauskova1997@mail.ru>
2023-02-12 16:35:09 +0300

    Добавили код программы

--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] ReadArray()
+{
+    Console.WriteLine("Введите элементы массива через пробел: ");
+    return Console.ReadLine().Split(" ");
+}
+
+string[] FilterArrayByLenght(string[] array, int lenght)
+{
+    int resultArrayLenght = 0;
+    int[] resultIndexes = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= lenght)
+        {
+            resultIndexes[resultArrayLenght] = i;
+            resultArrayLenght++;
+        }
+    }
+    string[] result = new string[resultArrayLenght];
+    if (resultArrayLenght != 0)
+    {
+        for (int i = 0; i < result.Length; i++)
+        {
+            result[i] = array[resultIndexes[i]];
+        }
+    }
+    return result;
+}
+
+void GoodPrint(string[] inputArray, string[] resultArray)
+{
+    string formattedString = $"[{String.Join(" , ", inputArray)}] -> [{String.Join(" , ", resultArray)}]";
+    Console.WriteLine(formattedString);
+}
+
+
+string[] inputArray = ReadArray();
+string[] result = FilterArrayByLenght(inputArray, 3);
+GoodPrint(inputArray, result);
